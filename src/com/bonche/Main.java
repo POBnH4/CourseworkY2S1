f172d80edb97beeb6e8b76115f7a53e71fb82b30
Peter Bonchev <bonchp4@yahoo.com>
2018-02-01 19:19:10 +0000

    Update Main.java

--- src/com/bonche/Main.java
+++ src/com/bonche/Main.java
@@ -6,8 +6,6 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        /** Before continuing READ the README file,
-         *  all the information + comments about the program are in it!**/
         Database database = new Database();
         EventQueue.invokeLater(() -> new LoginForm(database).setVisible(true));
     }
